c009eefa6c3d1e478c719babac46055663a3e912
Brea Borlas and julia sheremet <julia@>
2018-03-19 16:20:31 -0700

    Add practice examples for hw

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,108 @@ $(document).ready(function(){
     finalResult();
   });
 });
+
+
+
+////////////////////////////////////////////////////
+
+// var discount = function(){
+// 	var userInput = prompt("Find Out Your Savings for your 25% off Coupon, Enter Price for Product:");
+//   alert(userInput * .25);
+// }
+// discount();
+
+
+
+
+/* var leapModulo = function() {
+  var yearInput = parseInt(prompt("Please enter a random year"));
+  alert(yearInput%4);
+}
+
+leapModulo();
+ */
+
+
+
+
+/* var dogAge = prompt("how old is your dog? : ");
+
+var age = function(doggyAge){
+  alert(doggyAge * 7);
+}
+alert(age());
+
+age(dogAge);
+
+ */
+
+
+
+
+/*
+var character = function(){
+  var sentence = prompt("Enter a sentence: ");
+  return sentence.charAt(4);
+}
+alert(character());
+
+
+ */
+
+
+
+/*
+var character = "happy monday"
+
+var countCharacter = function(userInput){
+  alert(userInput.charAt(4));
+}
+countCharacter(character);
+ */
+
+
+
+
+
+
+/* var sentence = "Epicodus Week Two"
+
+var character = function(input) {
+  return input.charAt(4);
+}
+alert(character(sentence)); */
+
+
+
+
+/* var numbers = function(){
+  var userInput = prompt("Enter Any Number: ");
+return userInput.split("").reverse().join("");
+  alert(userInput.split("").reverse().join(""));
+}
+alert(numbers());
+
+numbers();
+ */
+
+
+
+/* var sentence = "quick brown fox";
+
+var upper = function(test1) {
+  return test1.toUpperCase();
+}
+
+alert(sentence.replace(/\b[a-z]{1}/g, upper));
+
+
+ */
+
+
+
+/* var alphabeticalOrder = function(){
+  var userInput = prompt("Enter a sentence: ");
+  return userInput.split("").sort().join("");
+}
+alert(alphabeticalOrder()); */
